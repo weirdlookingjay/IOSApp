3d3dc6cc9cdb62657f14d96aa678ac49d0c1b58e
Joe Clark <weirdlookingjay@live.com>
2022-08-23 11:55:31 -0400

    initial commit

---
+++ IOSApp/IOSApp/IOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAppApp.swift
+//  IOSApp
+//
+//  Created by Joe Clark on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct IOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
